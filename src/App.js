6bac155cfa2ff155bbaee158da51b5a63115c90f
AmanAgarwalX <amanagarwal9299@gmail.com>
2020-11-05 17:57:00 +0530

    Refactored code

--- src/App.js
+++ src/App.js
@@ -16,30 +16,7 @@ import _ from "lodash";
 import { KeyboardDatePicker } from "@material-ui/pickers";
 import { withSnackbar } from "notistack";
 import moment from "moment";
-
-const exportToJson = (objectData) => {
-  let filename = "export.json";
-  let contentType = "application/json;charset=utf-8;";
-  if (window.navigator && window.navigator.msSaveOrOpenBlob) {
-    var blob = new Blob(
-      [decodeURIComponent(encodeURI(JSON.stringify(objectData)))],
-      { type: contentType }
-    );
-    navigator.msSaveOrOpenBlob(blob, filename);
-  } else {
-    var a = document.createElement("a");
-    a.download = filename;
-    a.href =
-      "data:" +
-      contentType +
-      "," +
-      encodeURIComponent(JSON.stringify(objectData));
-    a.target = "_blank";
-    document.body.appendChild(a);
-    a.click();
-    document.body.removeChild(a);
-  }
-};
+import { isMultiline, form, exportToJson } from "./Form";
 
 const useStyles = makeStyles((theme) => ({
   icon: {
@@ -71,112 +48,8 @@ const useStyles = makeStyles((theme) => ({
     padding: 100,
     backgroundColor: theme.palette.primary.main,
   },
+  container: { backgroundColor: "white", padding: 20 },
 }));
-
-const form = [
-  {
-    first_name: {
-      type: "string",
-      required: true,
-      read_only: false,
-      label: "First name",
-      max_length: 30,
-    },
-    last_name: {
-      type: "string",
-      required: true,
-      read_only: false,
-      label: "Last name",
-      max_length: 150,
-    },
-    email: {
-      type: "email",
-      required: true,
-      read_only: false,
-      label: "Email",
-    },
-    address: {
-      type: "string",
-      required: true,
-      read_only: false,
-      label: "Address",
-    },
-    city: {
-      type: "string",
-      required: true,
-      read_only: false,
-      label: "City",
-    },
-    state: {
-      type: "string",
-      required: true,
-      read_only: false,
-      label: "State",
-    },
-    zip_code: {
-      type: "string",
-      required: true,
-      read_only: false,
-      label: "Zip code",
-    },
-    country: {
-      type: "string",
-      required: true,
-      read_only: false,
-      label: "Country",
-    },
-  },
-  {
-    phone_number: {
-      type: "string",
-      required: true,
-      read_only: false,
-      label: "Phone number",
-      max_length: 128,
-    },
-    type: {
-      type: "choice",
-      required: true,
-      read_only: false,
-      label: "Account Type",
-      choices: [
-        {
-          value: "Current",
-          display_name: "Current",
-        },
-        {
-          value: "Savings",
-          display_name: "Savings",
-        },
-      ],
-    },
-    pan: {
-      type: "string",
-      required: true,
-      read_only: false,
-      label: "Pan number",
-    },
-    aadhar: {
-      type: "string",
-      required: true,
-      read_only: false,
-      label: "Aadhar number",
-    },
-    company: {
-      type: "string",
-      required: true,
-      read_only: false,
-      label: "Current Company",
-    },
-    dob: {
-      type: "date",
-      required: true,
-      read_only: false,
-      label: "Date of Birth",
-    },
-  },
-];
-const isMultiline = new Set(["address"]);
 
 const QuestionAnswer = ({ question, answer }) => {
   const classes = useStyles();
@@ -264,7 +137,7 @@ export default withSnackbar((props) => {
 
   return (
     <div className={classes.mainGrid}>
-      <div style={{ backgroundColor: "white", padding: 20 }}>
+      <div className={classes.container}>
         <Stepper activeStep={step}>
           {steps.map((s) => (
             <Step key={s}>
---
+++ src/Form.js
@@ -0,0 +1,128 @@
+export const exportToJson = (objectData) => {
+  let filename = "export.json";
+  let contentType = "application/json;charset=utf-8;";
+  if (window.navigator && window.navigator.msSaveOrOpenBlob) {
+    var blob = new Blob(
+      [decodeURIComponent(encodeURI(JSON.stringify(objectData)))],
+      { type: contentType }
+    );
+    navigator.msSaveOrOpenBlob(blob, filename);
+  } else {
+    var a = document.createElement("a");
+    a.download = filename;
+    a.href =
+      "data:" +
+      contentType +
+      "," +
+      encodeURIComponent(JSON.stringify(objectData));
+    a.target = "_blank";
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+  }
+};
+
+export const form = [
+  {
+    first_name: {
+      type: "string",
+      required: true,
+      read_only: false,
+      label: "First name",
+      max_length: 30,
+    },
+    last_name: {
+      type: "string",
+      required: true,
+      read_only: false,
+      label: "Last name",
+      max_length: 150,
+    },
+    email: {
+      type: "email",
+      required: true,
+      read_only: false,
+      label: "Email",
+    },
+    address: {
+      type: "string",
+      required: true,
+      read_only: false,
+      label: "Address",
+    },
+    city: {
+      type: "string",
+      required: true,
+      read_only: false,
+      label: "City",
+    },
+    state: {
+      type: "string",
+      required: true,
+      read_only: false,
+      label: "State",
+    },
+    zip_code: {
+      type: "string",
+      required: true,
+      read_only: false,
+      label: "Zip code",
+    },
+    country: {
+      type: "string",
+      required: true,
+      read_only: false,
+      label: "Country",
+    },
+  },
+  {
+    phone_number: {
+      type: "string",
+      required: true,
+      read_only: false,
+      label: "Phone number",
+      max_length: 128,
+    },
+    type: {
+      type: "choice",
+      required: true,
+      read_only: false,
+      label: "Account Type",
+      choices: [
+        {
+          value: "Current",
+          display_name: "Current",
+        },
+        {
+          value: "Savings",
+          display_name: "Savings",
+        },
+      ],
+    },
+    pan: {
+      type: "string",
+      required: true,
+      read_only: false,
+      label: "Pan number",
+    },
+    aadhar: {
+      type: "string",
+      required: true,
+      read_only: false,
+      label: "Aadhar number",
+    },
+    company: {
+      type: "string",
+      required: true,
+      read_only: false,
+      label: "Current Company",
+    },
+    dob: {
+      type: "date",
+      required: true,
+      read_only: false,
+      label: "Date of Birth",
+    },
+  },
+];
+export const isMultiline = new Set(["address"]);
